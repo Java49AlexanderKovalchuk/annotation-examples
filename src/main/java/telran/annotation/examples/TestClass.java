d080e0b4036a9af7bf519b57068ba1b60a435d3d
AlexKovalchuk <kovalchuk111@gmail.com>
2023-10-30 01:36:37 +0200

    HW #54 solution from classwork - applying @BeforeEach and @Test

--- src/main/java/telran/annotation/examples/TestClass.java
+++ src/main/java/telran/annotation/examples/TestClass.java
@@ -4,19 +4,11 @@ import telran.test.annotation.BeforeEach;
 import telran.test.annotation.Test;
 
 public class TestClass {
-	@BeforeEach
-	void testBefore() {
-		System.out.println("exec 1 before test");
-	}
 	private void f1(int b) {
 		System.out.println("test f1 passed " + b);
 	}
 	private void f1() {
 		System.out.println("test f1 failed ");
-	}
-	@BeforeEach
-	void testBefore2() {
-		System.out.println("exec 2 before test");
 	}
 	@Test
 	void test1() {
@@ -25,5 +17,9 @@ public class TestClass {
 	@Test 
 	void test2() {
 		f1();
+	}
+	@BeforeEach
+	void setUp() {
+		System.out.println("set up");
 	}
 }
